c29f081edf1b037be417cd03b3de9b0360b5297d
Thomas Burkhardt <39870358+BurkyVIE@users.noreply.github.com>
2019-08-27 14:51:40 +0200

    Update app.R

--- app.R
+++ app.R
@@ -1,24 +1,17 @@
-#
-# This is a Shiny web application. You can run the application by clicking
-# the 'Run App' button above.
-#
-# Find out more about building applications with Shiny here:
-#
-#    http://shiny.rstudio.com/
-#
-
 library(shiny)
 
 ui <- fluidPage(
+  includeCSS(path = "style.css"),
   h1("Identifikation von EUR 2 Münzen"),
   textInput(inputId = "text", label = "Suche in ID"),
+  p("Suchstrings beziehen sich auf eine beliebige Übereinstimmung mit deneindeutigen IDs, deren Aufbau wie folgt aussieht '",strong("J J J J L L A 0"), "'. Dabei steht ", em("JJJJ fürs Prägejahr"), ", ", em("LL fürs Land"), ", ", em("A für die Münzart"), " und ", em("0 für die fortlaufende Nummer"), "."),
   tableOutput(outputId = "auswahl")
 )
 
 server <- function(input, output) {
   output$auswahl <- renderTable({
       coins %>%
-       filter(str_detect(ID, input$text)) %>% 
+       filter(str_detect(ID, tolower(input$text))) %>% 
        arrange(ID) %>%
        unnest() %>% 
        mutate(Ausgabe = as.character(Ausgabe)) %>% 
