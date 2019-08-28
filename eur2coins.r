bd2689fa65e7ba34d5b83e7accba45b04466aeaa
Thomas Burkhardt <39870358+BurkyVIE@users.noreply.github.com>
2019-08-28 12:28:59 +0200

    Update eur2coins.r

--- eur2coins.r
+++ eur2coins.r
@@ -1,9 +1,14 @@
 library(tidyverse)
-library(lubridate)
 
-read_delim("eur2coins_celex.txt", delim ="|", locale = locale(encoding = "WINDOWS-1252")) %>% 
-  mutate(Ausgabe = lubridate::ymd(Ausgabe),
-         Münzart = factor(Münzart, levels = c("g", "u"), labels = c("Gedenkmünze", "Umlaufmünze")),
+read_delim("eur2coins_celex.txt", delim ="|", locale = locale(encoding = "WINDOWS-1252"),
+           col_types = cols(Amtsblatt = col_character(),
+                            Land = col_character(),
+                            Prägejahr = col_integer(),
+                            Ausgabe = col_date(format = "%F"),
+                            Münzart = col_character(),
+                            Abbildung = col_character(),
+                            Münzzeichen = col_character())) %>% 
+  mutate(Münzart = factor(Münzart, levels = c("g", "u"), labels = c("Gedenkmünze", "Umlaufmünze")),
          Münzzeichen = str_split(Münzzeichen, pattern = ",")
          ) %>% 
   unnest() %>% 
@@ -11,7 +16,6 @@ read_delim("eur2coins_celex.txt", delim ="|", locale = locale(encoding = "WINDOW
   group_by(Land, Münzart, Prägejahr) %>% 
   mutate(ID = paste0(Prägejahr, Land, tolower(substr(Münzart, 1, 1)), cumsum(cs) %>% sprintf("%02d", .))) %>% 
   ungroup() %>% 
-  mutate(Prägejahr = as.integer(Prägejahr),
-         Land = toupper(Land), 
+  mutate(Land = toupper(Land), 
          Münzzeichen = na_if(Münzzeichen, "_")) %>% 
   select(Ausgabe, Münzart, Prägejahr, Land, Abbildung, Münzzeichen, Amtsblatt, ID) -> coins
