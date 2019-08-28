60d69c1f820dadb311b335cd384eb878fb2c859a
Thomas Burkhardt <39870358+BurkyVIE@users.noreply.github.com>
2019-08-28 12:29:12 +0200

    Update app.R

--- app.R
+++ app.R
@@ -6,41 +6,57 @@ ui <- fluidPage(
   includeCSS(path = "style.css"),
   h1("Identifikation von EUR 2 Münzen"),
   fluidRow(
-    column(3, 
-           textInput(inputId = "id", label = "Suche in ID")
-           ),
-    column(1,
-           tags$label("Löschen"), br(),
-           actionButton(inputId = "id_reset", label = "X")
-    ),
-    column(8,
-           p("Suchstrings beziehen sich auf eine beliebige Übereinstimmung mit der eindeutigen ID. Der Aufbau der ID lautet: ", code("JJJJLLA00"),
-             ", wobei ", code("JJJJ"), " fürs Prägejahr", ", ", code("LL"), " fürs Land", ", ", code("A"),
-             " für die Münzart", " und ", code("0"), " für die fortlaufende Nummer stehen. Die Verwendung von ", code("."), " als Joker ist zulässig.")
-           )
-    ),
-  fluidRow(
     column(3,
-           textInput(inputId = "abb", label = "Suche in Abbildung")
-           ),
-    column(1,
-           tags$label("Löschen"), br(),
-           actionButton(inputId = "abb_reset", label = "X")
-           )
+           h2("Suche"),
+           h3("in ID"),
+           wellPanel(
+             fluidRow(
+               column(9, textInput(inputId = "id", label = NULL)),
+               column(3, actionButton(inputId = "id_reset", label = "X"))
+               )
+             ),
+           p("Beliebige Übereinstimmung mit Feld", em("ID"), "; Aufbau ID: ", code("JJJJLLA00"),
+             ", wobei ", code("JJJJ"), " = Prägejahr", ", ", code("LL"), " = Land", ", ", code("A"),
+             " = Münzart", " und ", code("0"), " = fortlaufende Nummer.", code("."), " als Joker ist zulässig."),
+           h3("in Abbildung"),
+           wellPanel(
+             fluidRow(
+               column(9, textInput(inputId = "abb", label = NULL)),
+               column(3, actionButton(inputId = "abb_reset", label = "X"))
+               )
+             ),
+           p("Beliebige Übereinstimmung mit Feld Abbildung. Groß-/ Kleinschreibung wird ignoriert.")
     ),
-  tableOutput(outputId = "auswahl")
-)
+    column(9,
+           h2("Suchergebnisse"),
+           h3("Umlaufmünzen"),
+           tableOutput(outputId = "auswahl_u"),
+           h3("Gedenkmünzen"),
+           tableOutput(outputId = "auswahl_g")
+           )
+    )
+  )
 
 server <- function(input, output, session) {
-  output$auswahl <- renderTable({
+  output$auswahl_u <- renderTable({
       coins %>%
-       filter(str_detect(ID, tolower(input$id)), str_detect(Abbildung, input$abb)) %>% 
+       filter(Münzart == "Umlaufmünze", str_detect(ID, tolower(input$id)), str_detect(tolower(Abbildung), tolower(input$abb))) %>% 
        arrange(ID) %>%
        unnest() %>% 
        mutate(Ausgabe = as.character(Ausgabe)) %>% 
        rename(Mzz = Münzzeichen) %>% 
-       select(-Ausgabe)
+       select(-Ausgabe, -Münzart)
      })
+  
+  output$auswahl_g <- renderTable({
+    coins %>%
+      filter(Münzart == "Gedenkmünze", str_detect(ID, tolower(input$id)), str_detect(tolower(Abbildung), tolower(input$abb))) %>% 
+      arrange(ID) %>%
+      unnest() %>% 
+      mutate(Ausgabe = as.character(Ausgabe)) %>% 
+      rename(Mzz = Münzzeichen) %>% 
+      select(-Ausgabe, -Münzart)
+  })
   
   observeEvent(input$abb_reset, {
     updateTextInput(session, inputId = "abb", value = character(0))
