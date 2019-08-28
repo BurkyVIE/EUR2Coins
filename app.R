5f668b85b64f2db817c9d86fbb8ef464279df7d7
Thomas Burkhardt <39870358+BurkyVIE@users.noreply.github.com>
2019-08-28 10:05:17 +0200

    Update app.R

--- app.R
+++ app.R
@@ -1,25 +1,55 @@
 library(shiny)
 
+source("eur2coins.r")
+
 ui <- fluidPage(
   includeCSS(path = "style.css"),
   h1("Identifikation von EUR 2 Münzen"),
-  textInput(inputId = "text", label = "Suche in ID"),
-  p("Suchstrings beziehen sich auf eine beliebige Übereinstimmung mit deneindeutigen IDs, deren Aufbau wie folgt aussieht '",strong("J J J J L L A 0 0"), "'. Dabei steht ", em("JJJJ fürs Prägejahr"), ", ", em("LL fürs Land"), ", ", em("A für die Münzart"), " und ", em("0 für die fortlaufende Nummer"), "."),
+  fluidRow(
+    column(3, 
+           textInput(inputId = "id", label = "Suche in ID")
+           ),
+    column(1,
+           tags$label("Löschen"), br(),
+           actionButton(inputId = "id_reset", label = "X")
+    ),
+    column(8,
+           p("Suchstrings beziehen sich auf eine beliebige Übereinstimmung mit der eindeutigen ID. Der Aufbau der ID lautet: ", code("JJJJLLA00"),
+             ", wobei ", code("JJJJ"), " fürs Prägejahr", ", ", code("LL"), " fürs Land", ", ", code("A"),
+             " für die Münzart", " und ", code("0"), " für die fortlaufende Nummer stehen. Die Verwendung von ", code("."), " als Joker ist zulässig.")
+           )
+    ),
+  fluidRow(
+    column(3,
+           textInput(inputId = "abb", label = "Suche in Abbildung")
+           ),
+    column(1,
+           tags$label("Löschen"), br(),
+           actionButton(inputId = "abb_reset", label = "X")
+           )
+    ),
   tableOutput(outputId = "auswahl")
 )
 
-server <- function(input, output) {
+server <- function(input, output, session) {
   output$auswahl <- renderTable({
       coins %>%
-       filter(str_detect(ID, tolower(input$text))) %>% 
+       filter(str_detect(ID, tolower(input$id)), str_detect(Abbildung, input$abb)) %>% 
        arrange(ID) %>%
        unnest() %>% 
        mutate(Ausgabe = as.character(Ausgabe)) %>% 
        rename(Mzz = Münzzeichen) %>% 
        select(-Ausgabe)
      })
+  
+  observeEvent(input$abb_reset, {
+    updateTextInput(session, inputId = "abb", value = character(0))
+    })
+
+    observeEvent(input$id_reset, {
+    updateTextInput(session, inputId = "id", value = character(0))
+      })
 }
 
 # Run the application 
 shinyApp(ui = ui, server = server)
-
