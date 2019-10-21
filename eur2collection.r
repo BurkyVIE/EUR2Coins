172c66cb94b7fa6f9aae61679605da94ac12f568
Thomas Burkhardt <39870358+BurkyVIE@users.noreply.github.com>
2019-10-21 21:59:44 +0200

    Update eur2collection.r

--- eur2collection.r
+++ eur2collection.r
@@ -7,5 +7,5 @@ read_lines(file = "eur2collection.txt") %>%
          Tableau = (Zeilennummer - 1) %/% 24 %% 6 + 1,
          Spalte = (Zeilennummer - 1) %% 6 + 1,
          Zeile = (Zeilennummer - 1) %/% 6 %% 4 + 1,
-         Ablage = paste0(Box, Tableau, "|", Spalte, "x", Zeile, "(", Zeilennummer, ")")) %>% 
+         Ablage = paste0(Box, ".", Tableau, "|", Spalte, "x", Zeile, "(", Zeilennummer, ")")) %>% 
   left_join(coins %>% select(ID, Abbildung, Münzzeichen),by = "ID") -> collection
