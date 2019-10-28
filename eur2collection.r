ba587fc7f18199d0aec571f5ed79fd25fd861bc2
Thomas Burkhardt <39870358+BurkyVIE@users.noreply.github.com>
2019-10-28 20:12:25 +0100

    Update eur2collection.r

--- eur2collection.r
+++ eur2collection.r
@@ -2,7 +2,7 @@ library(tidyverse)
 
 read_lines(file = "eur2collection.txt") %>% 
   enframe(name = "Zeilennummer", value = "Sammlung") %>% 
-  separate(Sammlung, into = c("ID", "QualitÃ¤t"), sep = "-", convert = TRUE) %>%
+  separate(Sammlung, into = c("ID", "Qualität"), sep = "-", convert = TRUE) %>%
   mutate(Box = (Zeilennummer - 1)  %/% 144 + 1,
          Tableau = (Zeilennummer - 1) %/% 24 %% 6 + 1,
          Spalte = (Zeilennummer - 1) %% 6 + 1,
