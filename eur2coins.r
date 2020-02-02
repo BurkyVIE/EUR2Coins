a33f629124fbc3e4431c6dcd7d43d3b094be6f62
Thomas Burkhardt <39870358+BurkyVIE@users.noreply.github.com>
2020-02-02 01:15:49 +0100

    Update eur2coins.r

--- eur2coins.r
+++ eur2coins.r
@@ -1,20 +1,28 @@
 library(tidyverse)
 
-read_delim("eur2coins_celex.txt", delim ="|", locale = locale(encoding = "UTF-8"),
-           col_types = cols(Amtsblatt = col_character(),
-                            Land = col_character(),
-                            Prägejahr = col_integer(),
-                            Ausgabe = col_date(format = "%F"),
-                            Münzart = col_character(),
-                            Abbildung = col_character(),
-                            Münzzeichen = col_character())) %>% 
-  mutate(Münzart = factor(Münzart, levels = c("g", "u"), labels = c("Gedenkmünze", "Umlaufmünze")),
-         Münzzeichen = str_split(Münzzeichen, pattern = ",")) %>% 
-  unnest(cols = c(Münzzeichen)) %>%   # Erweitern um die Münzzeichen
-  add_column(cs = 1) %>% 
-  group_by(Land, Münzart, Prägejahr) %>% 
-  mutate(ID = paste0(Prägejahr, Land, tolower(substr(Münzart, 1, 1)), cumsum(cs) %>% sprintf("%02d", .))) %>% 
-  ungroup() %>% 
-  mutate(Land = toupper(Land), 
-         Münzzeichen = coalesce(Münzzeichen, "")) %>% 
+read_delim("eur2coins_celex.txt",
+  delim = "|", locale = locale(encoding = "UTF-8"),
+  col_types = cols(
+    Amtsblatt = col_character(),
+    Land = col_character(),
+    Prägejahr = col_integer(),
+    Ausgabe = col_date(format = "%F"),
+    Münzart = col_character(),
+    Abbildung = col_character(),
+    Münzzeichen = col_character()
+  )
+) %>%
+  mutate(
+    Münzart = factor(Münzart, levels = c("g", "u"), labels = c("Gedenkmünze", "Umlaufmünze")),
+    Münzzeichen = str_split(Münzzeichen, pattern = ",")
+  ) %>%
+  unnest(cols = c(Münzzeichen)) %>% # Erweitern um die Münzzeichen
+  add_column(cs = 1) %>%
+  group_by(Land, Münzart, Prägejahr) %>%
+  mutate(ID = paste0(Prägejahr, Land, tolower(substr(Münzart, 1, 1)), cumsum(cs) %>% sprintf("%02d", .))) %>%
+  ungroup() %>%
+  mutate(
+    Land = toupper(Land),
+    Münzzeichen = coalesce(Münzzeichen, "")
+  ) %>%
   select(Ausgabe, Münzart, Prägejahr, Land, Abbildung, Münzzeichen, Amtsblatt, ID) -> coins
