843f880d7fdcc479450627ded8782e88125780fe
Thomas Burkhardt <39870358+BurkyVIE@users.noreply.github.com>
2020-02-02 01:16:00 +0100

    Update eur2collection.r

--- eur2collection.r
+++ eur2collection.r
@@ -1,11 +1,13 @@
 library(tidyverse)
 
-read_lines(file = "eur2collection.txt") %>% 
-  enframe(name = "Zeilennummer", value = "Sammlung") %>% 
+read_lines(file = "eur2collection.txt") %>%
+  enframe(name = "Zeilennummer", value = "Sammlung") %>%
   separate(Sammlung, into = c("ID", "Qualität"), sep = "-", convert = TRUE) %>%
-  mutate(Box = (Zeilennummer - 1)  %/% 144 + 1,
-         Tableau = (Zeilennummer - 1) %/% 24 %% 6 + 1,
-         Spalte = (Zeilennummer - 1) %% 6 + 1,
-         Zeile = (Zeilennummer - 1) %/% 6 %% 4 + 1,
-         Ablage = paste0(Box, Tableau, Spalte, Zeile, "x", sprintf("%04d", Zeilennummer))) %>% 
-  left_join(coins %>% select(ID, Abbildung, Münzzeichen),by = "ID") -> collection
+  mutate(
+    Box = (Zeilennummer - 1) %/% 144 + 1,
+    Tableau = (Zeilennummer - 1) %/% 24 %% 6 + 1,
+    Spalte = (Zeilennummer - 1) %% 6 + 1,
+    Zeile = (Zeilennummer - 1) %/% 6 %% 4 + 1,
+    Ablage = paste0(Box, Tableau, Spalte, Zeile, "x", sprintf("%04d", Zeilennummer))
+  ) %>%
+  left_join(coins %>% select(ID, Abbildung, Münzzeichen), by = "ID") -> collection
