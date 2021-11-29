ef7025ad4a0c7ee0e8a431806b63ea4dfd2e2030
Thomas Burkhardt <39870358+BurkyVIE@users.noreply.github.com>
2021-11-29 10:12:53 +0100

    Update eur2coins.r

--- eur2coins.r
+++ eur2coins.r
@@ -1,31 +1,31 @@
 library(tidyverse)
 
 read_delim("eur2coins_celex.txt",
-  delim = "|", locale = locale(encoding = "UTF-8"),
+  delim = "|", locale = locale(encoding = "UTF-8"), lazy = FALSE,
   col_types = cols(
     Amtsblatt = col_character(),
     Land = col_character(),
-    Prägejahr = col_integer(),
+    PrÃ¤gejahr = col_integer(),
     # Ausgabe = col_date(format = "%F"), # no leading zeros - problem with parsing
     Ausgabe = col_character(),
-    Münzart = col_character(),
+    MÃ¼nzart = col_character(),
     Abbildung = col_character(),
-    Münzzeichen = col_character()
+    MÃ¼nzzeichen = col_character()
   ),
   lazy = FALSE
 ) %>%
   mutate(
     Ausgabe = lubridate::ymd(Ausgabe),
-    Münzart = factor(Münzart, levels = c("g", "u"), labels = c("Gedenkmünze", "Umlaufmünze")),
-    Münzzeichen = str_split(Münzzeichen, pattern = ",")
+    MÃ¼nzart = factor(MÃ¼nzart, levels = c("g", "u"), labels = c("GedenkmÃ¼nze", "UmlaufmÃ¼nze")),
+    MÃ¼nzzeichen = str_split(MÃ¼nzzeichen, pattern = ",")
   ) %>%
-  unnest(cols = c(Münzzeichen)) %>% # Erweitern um die Münzzeichen
+  unnest(cols = c(MÃ¼nzzeichen)) %>% # Erweitern um die MÃ¼nzzeichen
   add_column(cs = 1) %>%
-  group_by(Land, Münzart, Prägejahr) %>%
-  mutate(ID = paste0(Prägejahr, Land, tolower(substr(Münzart, 1, 1)), cumsum(cs) %>% sprintf("%02d", .))) %>%
+  group_by(Land, MÃ¼nzart, PrÃ¤gejahr) %>%
+  mutate(ID = paste0(PrÃ¤gejahr, Land, tolower(substr(MÃ¼nzart, 1, 1)), cumsum(cs) %>% sprintf("%02d", .))) %>%
   ungroup() %>%
   mutate(
     Land = toupper(Land),
-    Münzzeichen = coalesce(Münzzeichen, "")
+    MÃ¼nzzeichen = coalesce(MÃ¼nzzeichen, "")
   ) %>%
-  select(Ausgabe, Münzart, Prägejahr, Land, Abbildung, Münzzeichen, Amtsblatt, ID) -> coins
+  select(Ausgabe, MÃ¼nzart, PrÃ¤gejahr, Land, Abbildung, MÃ¼nzzeichen, Amtsblatt, ID) -> coins
