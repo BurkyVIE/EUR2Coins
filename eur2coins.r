7165ef742b5c5797ed4bdf6985a6c81755712f3d
Thomas Burkhardt <39870358+BurkyVIE@users.noreply.github.com>
2024-03-22 09:43:28 +0100

    Update eur2coins.r

--- eur2coins.r
+++ eur2coins.r
@@ -1,30 +1,44 @@
+# LIBRARIES ----
 library(tidyverse)
 
-read_delim("eur2coins_celex.txt",
-  delim = "|", locale = locale(encoding = "UTF-8"), lazy = FALSE,
-  col_types = cols(
-    Amtsblatt = col_character(),
-    Land = col_character(),
-    Prägejahr = col_integer(),
-    # Ausgabe = col_date(format = "%F"), # no leading zeros - problem with parsing
-    Ausgabe = col_character(),
-    Münzart = col_character(),
-    Abbildung = col_character(),
-    Münzzeichen = col_character()
-  ),
-) %>%
-  mutate(
-    Ausgabe = lubridate::ymd(Ausgabe),
-    Münzart = factor(Münzart, levels = c("g", "u"), labels = c("Gedenkmünze", "Umlaufmünze")),
-    Münzzeichen = str_split(Münzzeichen, pattern = ",")
-  ) %>%
-  unnest(cols = c(Münzzeichen)) %>% # Erweitern um die Münzzeichen
-  add_column(cs = 1) %>%
-  group_by(Land, Münzart, Prägejahr) %>%
-  mutate(ID = paste0(Prägejahr, Land, tolower(substr(Münzart, 1, 1)), cumsum(cs) %>% sprintf("%02d", .))) %>%
-  ungroup() %>%
-  mutate(
-    Land = toupper(Land),
-    Münzzeichen = coalesce(Münzzeichen, "")
-  ) %>%
+# GLOBAL ----
+## where is directory ----
+directory <- ""
+
+## import celex_*.txt ----
+import_celex <- function(file) {
+  read_delim(paste0(directory,file), delim ="|", locale = locale(encoding = "UTF-8"), lazy = FALSE,
+             col_types = cols(Amtsblatt = col_character(),
+                              Land = col_character(),
+                              Prägejahr = col_integer(),
+                              Ausgabe = col_date(format = "%F"),
+                              Münzart = col_character(),
+                              Abbildung = col_character(),
+                              Münzzeichen = col_character()))
+}
+
+# IMPORT ----
+## get list of files ----
+filelist <- dir(directory)
+filelist <- filelist[startsWith(filelist, "eur2coins_celex")]
+
+## do import
+raw <- map_df(filelist, ~import_celex(.))
+
+# TIDY ----
+## do tidy ----
+raw %>% 
+  mutate(Münzart = factor(Münzart, levels = c("g", "u"), labels = c("Gedenkmünze", "Umlaufmünze")),
+         Münzzeichen = str_split(Münzzeichen, pattern = ",")) %>% 
+  unnest(Münzzeichen) %>% # Erweitern um die Münzzeichen
+  add_column(cs = 1) %>% # Hilfsvariable für Durchnumerierung (ID)
+  group_by(Land, Münzart, Prägejahr) %>% 
+  mutate(ID = paste0(Prägejahr, Land, tolower(substr(Münzart, 1, 1)), cumsum(cs) %>% sprintf("%02d", .))) %>% 
+  ungroup() %>% 
+  mutate(Land = toupper(Land), 
+         Münzzeichen = coalesce(Münzzeichen, "")) %>% 
   select(Ausgabe, Münzart, Prägejahr, Land, Abbildung, Münzzeichen, Amtsblatt, ID) -> coins
+
+# CLEAN UP ----
+##do clean up ----
+rm(directory, import_celex, filelist, raw)
