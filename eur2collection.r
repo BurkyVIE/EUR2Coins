87e44168b3439bdd901fb507fcb9244d89bc70ee
Thomas Burkhardt <39870358+BurkyVIE@users.noreply.github.com>
2025-01-22 10:39:18 +0100

    Add files via upload

--- eur2collection.r
+++ eur2collection.r
@@ -8,5 +8,5 @@ read_lines(file = "eur2collection.txt", lazy = FALSE) |>
     Tableau = (Zeilennummer - 1) %/% 24 %% 6 + 1,
     Zeile = (Zeilennummer - 1) %/% 6 %% 4 + 1,
     Spalte = (Zeilennummer - 1) %% 6 + 1,
-    Ablage = paste0(Box, Tableau, Zeile, Spalte, "×", sprintf("%04d", Zeilennummer))) |> # &times; = Alt+0215
+    Ablage = paste0(Box, Tableau, Zeile, Spalte, "×", str_pad(Zeilennummer, 4, pad = "0"))) |> #sprintf("%04d", Zeilennummer))) |> # &times; = Alt+0215
   left_join(coins |> select(ID, Abbildung, Münzzeichen), by = "ID") -> collection
