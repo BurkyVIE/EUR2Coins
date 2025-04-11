ad45c8b8d63fc63a895360afd2669dbddc764f3a
Thomas Burkhardt <39870358+BurkyVIE@users.noreply.github.com>
2025-04-11 06:45:01 +0200

    Update app.R

--- app.R
+++ app.R
@@ -371,11 +371,11 @@ server <- function(input, output, session) {
   
   ## Funktion zum Formatieren der Häufigkeit ----
   form_hfgkt <- function(txt) {
-    c("<div style='font-size: 1.1em; background-color: #b22222; color: white'>⇓</div>",
-      "<div style='font-size: 1.1em; background-color: #c56320; color: white'>↓</div>", # ⇘
-      "<div style='font-size: 1.1em; background-color: #daa520; color: white'>-</div>", # ⇒
-      "<div style='font-size: 1.1em; background-color: #7d9820; color: white'>↑</div>", # ⇗
-      "<div style='font-size: 1.1em; background-color: #228b22; color: white'>⇑</div>")[txt]
+    c("<div style='font-size: 1.1em; background-color: #b22222; color: white'>⩔</div>",
+      "<div style='font-size: 1.1em; background-color: #c56320; color: white'>˅</div>",
+      "<div style='font-size: 1.1em; background-color: #daa520; color: white'>-</div>",
+      "<div style='font-size: 1.1em; background-color: #7d9820; color: white'>˄</div>",
+      "<div style='font-size: 1.1em; background-color: #228b22; color: white'>⩓</div>")[txt]
   }
   
   ## Funktion zur Darstellung der Daten ----
