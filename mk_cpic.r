62559b4e34c948984c9779934485cfa07f0e02c4
Thomas Burkhardt <39870358+BurkyVIE@users.noreply.github.com>
2025-09-03 19:05:50 +0200

    Update mk_cpic.r

--- mk_cpic.r
+++ mk_cpic.r
@@ -6,7 +6,7 @@ select(coins, Amtsblatt, Land, ID) |>
                        str_sub(Land, 1, 1),
                        str_remove_all(Amtsblatt, "[\\D]"),
                        str_sub(Land, 2, 2),
-                       ".png"),
+                       ".jpg"),
          Exists = map_lgl(PicFile, ~file.exists(.))) |> 
   filter(Exists) |> 
   unnest(cols = IDs) -> cpic
