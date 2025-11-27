a297c810482e7c987c04b311ba4ee1fbe9c7da5c
Thomas Burkhardt <39870358+BurkyVIE@users.noreply.github.com>
2025-11-27 13:07:15 +0100

    Update missing_cpic.r

--- missing_cpic.r
+++ missing_cpic.r
@@ -4,5 +4,9 @@ filter(cpic, !Exists) |>
             Jahr = as.integer(str_sub(ID, 1, 4)),
             Abbildung,
             Filename = str_extract(PicFile, "[A-Z]\\d*[A-Z]")) |>
-  unique() |>
-  arrange(Land, Jahr)
+  unique() |> 
+  group_by(Land, Abbildung, Filename) |> 
+  summarise(min_Jahr = min(Jahr)) |> 
+  relocate(min_Jahr, .after = Land) |>
+  arrange(Land, min_Jahr) 
+  
